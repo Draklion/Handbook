1ed8a955c6ad28944bb8512ffc1dc22ff53868bc
Dmitrii Machnev <dam-11-08-88@mail.ru>
2022-11-07 21:51:19 +0300

    Added changes.

--- HandBook/user_interface.py
+++ HandBook/user_interface.py
@@ -7,7 +7,7 @@ from tkinter.scrolledtext import ScrolledText
 
 root = tk.Tk()  
 root.title("Автопарк")
-root.geometry("500x500+300+150")
+root.geometry("700x500+250+250")
 cb = Combobox(root, values=("Нет","С568АО","В233АМ","С045РО","О002ОА"),justify=CENTER)
 cb.current(0)
 cb.pack()
@@ -22,26 +22,30 @@ def single_click():
         buses_reader = csv.DictReader(buses_file, delimiter=";")
         for buses_row in buses_reader:
             if cb.get() == buses_row['State_number']:
-                    ST.insert(END, "Информация об автобусе: ")
-                    ST.insert(END, f"{buses_row['Serial_number'], buses_row['State_number'], buses_row['ID_driver'], buses_row['ID_conductor'], buses_row['ID_Maintenance']}\n")
-                    ID_driver = buses_row['ID_driver']
-                    ID_Maintenance = buses_row['ID_Maintenance']
-    
+                ST.insert(END, "Информация об автобусе: ")
+                ST.insert(END, f"{buses_row['Serial_number'], buses_row['State_number'], buses_row['ID_driver'], buses_row['ID_conductor'], buses_row['ID_Maintenance']}\n")
+                ID_driver = buses_row['ID_driver']
+                ID_Maintenance = buses_row['ID_Maintenance']
+                log.Buses(f'Open and close file "Buses.csv", State_number:{buses_row["State_number"]}, ID_driver: {ID_driver}, ID_Maintenance: {ID_Maintenance}\n')
+        
     with open("Drivers.csv",encoding="utf-8",newline="") as drivers_file:
         drivers_reader = csv.DictReader(drivers_file, delimiter=";")
         for drivers_row in drivers_reader:
             if ID_driver == drivers_row['ID_driver']:
                 ST.insert(END, "Информация о водителе: ")
                 ST.insert(END, f"{drivers_row['ID_driver'], drivers_row['Full_name'], drivers_row['Experience'], drivers_row['Open categories']}\n")
-    
+                log.Drivers(f'Open and close file "Drivers.csv", Full_name:{drivers_row["Full_name"]}, ID_driver: {ID_driver}\n')
+                
     with open("PTI.csv",encoding="utf-8",newline="") as pti_file:
         pti_reader = csv.DictReader(pti_file, delimiter=";")
         for pti_row in pti_reader:
             if ID_Maintenance == pti_row['ID_Maintenance']:
                 ST.insert(END, "Информация о ТО: ")
-                ST.insert(END, f"{pti_row['ID_Maintenance'], pti_row['Date_passage'], pti_row['ID_works']}")                
+                ST.insert(END, f"{pti_row['ID_Maintenance'], pti_row['Date_passage'], pti_row['ID_works']}")
+                log.PTI(f'Open and close file "PTI.csv", Date_passage:{pti_row["Date_passage"]}, ID_Maintenance: {ID_Maintenance}\n')
+                
     return
 
-tk.Button(root, text="Вывести результат", command=single_click, font=10,).place(x=150, y=450)
+tk.Button(root, text="Вывести результат", command=single_click, font=10,).pack(side=BOTTOM,pady=10)
 
 root.mainloop()
